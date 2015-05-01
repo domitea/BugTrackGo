d96faa9c4c6d00dffe8583a7dfa1c3bf304633bc
Dominik Matoulek <domitea@gmail.com>
2015-05-01 17:23:21 +0200

    adding new controlers

---
+++ api/controllers/BugController.js
@@ -0,0 +1,11 @@
+/**
+ * BugController
+ *
+ * @description :: Server-side logic for managing bugs
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
