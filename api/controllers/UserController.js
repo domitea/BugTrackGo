80d260daa802ac2840caefc167710549cbe45735
Dominik Matoulek <domitea@gmail.com>
2015-05-09 21:51:54 +0200

    Adding bugs list on home page

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -1,3 +1,4 @@
+var Q = require('q');
 /**
  * UserController
  *
@@ -7,7 +8,23 @@
 
 module.exports = {
 	home: function (req, res) {
-				
-		res.view();
+
+		Q.all([
+			Bug.find({creator: res.locals.user.id, solved: false}).then(),
+			Bug.find({assignedTo: res.locals.user.id, solved: false}).then(),
+		]).spread(function (myBugs, assignedBugs) {
+
+			var bugs = [];
+
+			for (var i = 0; i < assignedBugs.length; i++)
+			{
+				if (assignedBugs[i].creator != res.locals.user.id)
+				{
+					bugs.push(assignedBugs[i]);
+				}
+			}
+
+			res.view({bugs: myBugs, assignedBugs: bugs});
+		}).fail(function (why) { res.serverError(why)});
 	}
 };
